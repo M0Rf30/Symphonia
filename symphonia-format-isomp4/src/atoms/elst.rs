2dd555f5be318e5b749688b15a637db04fe76d6d
Gianluca Boiano <morf3089@gmail.com>
2026-09-23 15:08:19 +0200

    isomp4: Expose edit list entry fields for gapless playback support.

--- symphonia-format-isomp4/src/atoms/elst.rs
+++ symphonia-format-isomp4/src/atoms/elst.rs
@@ -11,20 +11,20 @@ use crate::atoms::{Atom, AtomHeader, AtomIterator, ReadAtom, Result};
 use crate::atoms::{decode_error, limits::*};
 
 /// Edit list entry.
-#[allow(dead_code)]
 #[derive(Debug)]
 pub struct ElstEntry {
-    segment_duration: u64,
-    media_time: i64,
-    media_rate_int: i16,
-    media_rate_frac: i16,
+    pub segment_duration: u64,
+    pub media_time: i64,
+    #[allow(dead_code)]
+    pub media_rate_int: i16,
+    #[allow(dead_code)]
+    pub media_rate_frac: i16,
 }
 
 /// Edit list atom.
-#[allow(dead_code)]
 #[derive(Debug)]
 pub struct ElstAtom {
-    entries: Vec<ElstEntry>,
+    pub entries: Vec<ElstEntry>,
 }
 
 impl Atom for ElstAtom {
